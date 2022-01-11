02754d402763aa97e9c5710d096465bf3a30d5f9
david fletcher <davidfletcheraaron@gmail.com>
2022-01-11 12:36:59 -0500

    getting all Posts

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,8 +1,20 @@
 // implement your posts router here
-const express = require('express')
+const express = require('express');
+const Post = require('./posts-model')
 
 const router = express.Router()
 
-
+router.get('/', async (req, res) =>{
+   const allPosts = await Post.find(req.query)
+   try{
+      if ( !allPosts ){
+         res.status(404).json({ message: 'no users found' })
+      } else{
+         res.json(allPosts)
+      }
+   } catch(err){
+      res.status(500).json({ message: err.message })
+   }
+})
 
 module.exports = router
