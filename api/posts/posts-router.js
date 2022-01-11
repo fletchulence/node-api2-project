66980b15c85889b67fdf240ba86d5464728836bb
david fletcher <davidfletcheraaron@gmail.com>
2022-01-11 12:51:30 -0500

    the id is working and returning the exp resp

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -4,6 +4,7 @@ const Post = require('./posts-model')
 
 const router = express.Router()
 
+// [GET] all posts
 router.get('/', async (req, res) =>{
    const allPosts = await Post.find(req.query)
    try{
@@ -13,6 +14,21 @@ router.get('/', async (req, res) =>{
          res.json(allPosts)
       }
    } catch(err){
+      res.status(500).json({ message: err.message })
+   }
+})
+
+router.get('/:id', async (req, res)=>{
+   const { id } = req.params
+   const postById = await Post.findById( id )
+   try{
+      if ( !postById ){
+         res.status(404).json({ message: 'this id does not exist'})
+      } else {
+         res.json( postById )
+      }
+   } catch(err){
+      console.log(`id`, id)
       res.status(500).json({ message: err.message })
    }
 })
